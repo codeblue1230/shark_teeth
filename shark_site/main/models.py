2cf79ca6fa9890a5b5ef016c4a49a1a5d09ca0b8
Marc Engle <marcengle30@gmail.com>
2023-11-20 22:04:55 -0500

    added admin page and Post Model to db

--- shark_site/main/models.py
+++ shark_site/main/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.CharField(max_length=1000)
+    picture = models.ImageField(upload_to=('files/teeth_images'))
+    
--- shark_site/main/views.py
+++ shark_site/main/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 def home(response):
     return render(response, "main/home.html", {})
+
+def forums(resposne):
+    all_posts = Post.objects.all()
+    for p in all_posts:
+        print(p.title)
+        print(p.text)
+    return render(resposne, "main/forums.html", {})
