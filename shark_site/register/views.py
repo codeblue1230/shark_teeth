67fc6e27321350cf808ca0683ece58ee7cd530ed
Marc Engle <marcengle30@gmail.com>
2023-11-27 19:56:19 -0500

    added login template and date_joined to User model

--- shark_site/register/views.py
+++ shark_site/register/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render, redirect
+from django.utils import timezone
+
 from .forms import RegisterForm
 
 def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
-            form.save()
+            user = form.save(commit=False)  # Get the unsaved user instance
+            user.date_joined = timezone.now()  # Set the date_joined field of the user
+            user.save()  # Save the user instance with the updated date_joined
         return redirect("/")
 
     else:
--- shark_site/shark_site/urls.py
+++ shark_site/shark_site/urls.py
@@ -24,7 +24,8 @@ from register import views as v
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
-    path("register/", v.register, name="register")
+    path("", include("django.contrib.auth.urls")),
+    path("register/", v.register, name="register"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Referenced from settings.py
